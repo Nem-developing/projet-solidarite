3cfd624d9eabefe48c481fc847e5e20a337add0d
Dorian Rastello <49765091+Neyrim83@users.noreply.github.com>
2020-06-03 02:04:18 +0200

    Work on aesthetics

--- pages/bonus.php
+++ pages/bonus.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="author" content="Néhémie Barkia, Ilan Marrache, Dorian Rastello.">
+        <meta name="author" content="Néhémie Barkia, Ilan Marrache, Dorrian Rastello.">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="icon" href="../favicon.ico">
         <title>Les Bonus du BTS Communication</title>
@@ -19,13 +19,83 @@
         <?php
           include '../includes/header.html';
         ?>
+        
+        <! -- Bandeau Titre -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" id="bandeaux">
+            
+                    <! -- Menu -->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light brm-headerTop" id="menuprincipal">
+                    <img src="../logo/logo-1.png" class="imageslogo1">
+        <div class="container">
+        <a class="navbar-brand" href="#">BONAPARTE - SOLIDAIRE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    
 
-        <! -- Fil d'Ariane -->
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="../index.html">Accueil</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Les Bonus du BTS Communication</li>
-            </ol>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Productions
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="index.html">Bonaparte solidaire de nos héros du quotidient !</a>
+                            <a class="dropdown-item" href="./pages/eleves.html">Productions des élèves</a>
+                            <a class="dropdown-item" href="./pages/familles.html">Productions des familles</a>
+                            <a class="dropdown-item" href="./pages/personnels.html">Productions des personnels du Lycée</a>
+                            <a class="dropdown-item" href="./pages/poesie.html">Moments de poésie</a>
+                            <a class="dropdown-item" href="./pages/Remerciements des Héros du quotidien.html">Remerciements des Héros du quotidien</a>
+                            <a class="dropdown-item" href="./pages/Les Vidéos.html">Les Vidéos</a>
+                            <a class="dropdown-item" href="./pages/Ils vous soutiennent.html">Ils vous soutiennent</a>
+                            <a class="dropdown-item" href="./pages/Projet fabriquons des masques - Vie Scolaire.html">Projet fabriquons des masques - Vie Scolaire</a>
+                            <a class="dropdown-item" href="./pages/actions.html">Les actions solidaires</a>
+                            <a class="dropdown-item" href="./pages/bonus.html">Les Bonus du BTS Communication</a>
+                            <a class="dropdown-item" href="./pages/reseau.html">Le Réseau Bonaparte</a>
+                            <a class="dropdown-item" href="./pages/communication.html">Retours de Presse et communication</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Actions
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Projet fabriquons des masques - Vie Scolaire</a>
+                            <a class="dropdown-item" href="pages/actions.html">Les actions solidaires</a>
+                            <a class="dropdown-item" href="pages/reseau.html">Le Réseau Bonaparte</a>
+                        </div>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Autre
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="pages/communication.html">Retours de Presse et communication</a>
+                            <a class="dropdown-item" href="pages/bonus.html">Les Bonus du BTS Communication</a>
+                        </div>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+                    <img src="../logo/logo2.png" class="imageslogo2">
+                </nav>
+            
+            
+            
+                    <h3>Covid19 - Le Lycée Bonaparte de Toulon, sa communauté scolaire et son réseau se mobilisent pour adresser leurs remerciements et leur soutien à nos héros du quotidien.</h3>
+            
+            <nav class="brm-headerBottom" id="bandeaux-bottom">
+                <h5>Bonaparte solidaire de nos héros du quotidien !</h5>
+            </nav>
         </nav>
         
         <! -- Caroussel (Slide Show) -->
@@ -106,59 +176,118 @@
 
         </div>
         
-
-        <! -- Affichage des images -->
-        <div class="card-group">
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 1.jpg" class="card-img-top" alt="...">
+                <! -- Affichage des images -->
+        
+    <div class="container fond bonus">
+        <div class="row image1">
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 1.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 2.jpg" class="card-img-top" alt="...">
+        
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 2.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 3.jpg" class="card-img-top" alt="...">
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 3.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 4.jpg" class="card-img-top" alt="...">
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 4.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+                </div>
+        </div>
+        
+        
+        <div class="row image1">
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 5.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 5.jpg" class="card-img-top" alt="...">
+        
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 6.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+            </div>
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 7.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+            </div>
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 8.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+                </div>
+        </div>
+        
+        
+        <div class="row image1">
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 9.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+            </div>
+        
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 10.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+            </div>
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 11.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
+            </div>
+            
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 12.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
         </div>
         
-        <div class="card-group">
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 6.jpg" class="card-img-top" alt="...">
+        <div class="row image1">
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="./../images/les_bonus_du_bts_communication/Bonus 13.jpg" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 7.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 8.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 9.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 10.jpg" class="card-img-top" alt="...">
+        
+            <div class="col">
+                    <a class="a-img-txt" href="">
+                        <img src="../images/les_bonus_du_bts_communication/Vidéo BTS COM.mp4" class="card-img-top" alt="...">
+                        <span class="a-txt cb"></span>
+                    </a>
             </div>
         </div>
         
-        <div class="card-group">
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 11.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 12.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Bonus 13.jpg" class="card-img-top" alt="...">
-            </div>
-            <div class="card">
-                <img src="./../images/les_bonus_du_bts_communication/Vidéo BTS COM.mp4" class="card-img-top" alt="...">
-            </div>
-        </div>
-        
+ </div>
 
 
 
@@ -171,6 +300,6 @@
     </body>
     
     <footer>
-        <p>&copy;2020 Projet Solidarité - <a href="https://github.com/IlanMarrache">Ilan Marrache</a> - <a href="https://github.com/Neyrim83">Dorian Rastello - <a href="http://github.com/nem-developing/">Néhémie Barkia</a><p>
+        <p>&copy;2020 Projet Solidarité <a href="https://github.com/IlanMarrache" class="link">Ilan Marrache</a> - <a href="https://github.com/Neyrim83" class="link">Dorian Rastello</a> - <a href="http://github.com/nem-developing/" class="link">Néhémie Barkia</a><p>
     </footer>
 </html>
