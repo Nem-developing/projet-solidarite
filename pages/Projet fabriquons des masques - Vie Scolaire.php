049eead50a84b03cd5d2a7004d21df5beb51cbcb
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-06-04 12:18:30 +0200

    Update Projet fabriquons des masques - Vie Scolaire.php

--- pages/Projet fabriquons des masques - Vie Scolaire.php
+++ pages/Projet fabriquons des masques - Vie Scolaire.php
@@ -20,13 +20,6 @@
           include '../includes/header.php';
         ?>
 
-        <! -- Fil d'Ariane -->
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="../index.html">Accueil</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Elèves</li>
-            </ol>
-        </nav>
         
         <! -- Caroussel (Slide Show) -->
          
