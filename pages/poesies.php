6a8630e75f48eaacab1ba25b3d492b5e9962cd94
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-04 22:50:50 +0200

    Update poesies.php

--- pages/poesies.php
+++ pages/poesies.php
@@ -11,7 +11,6 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="icon" href="../favicon.ico">
         <title>Moments de poésie</title>
-        <link href="../css/poesies.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
 
