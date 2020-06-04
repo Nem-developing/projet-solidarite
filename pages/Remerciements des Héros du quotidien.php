b1a16b6c3afefe492fc82a555527c5a7fd0305f6
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-06-04 12:18:41 +0200

    Update Remerciements des Héros du quotidien.php

--- pages/Remerciements des Héros du quotidien.php
+++ pages/Remerciements des Héros du quotidien.php
@@ -21,14 +21,7 @@
         ?>
        
 
-        <! -- Fil d'Ariane -->
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="../index.php">Accueil</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Elèves</li>
-            </ol>
-        </nav>
-        
+                
         <! -- Caroussel (Slide Show) -->
          
         <div class="car-class">
