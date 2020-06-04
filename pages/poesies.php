e622bcafc6380a07d27e50c56c83a9b8e6da0f63
IlanMarrache <65493510+IlanMarrache@users.noreply.github.com>
2020-06-04 23:12:47 +0200

    Suppression du Border

--- pages/poesies.php
+++ pages/poesies.php
@@ -77,16 +77,6 @@
         </div>
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         <! -- Affichage des images -->
         <div class="container fond" id="full-back">
         <div class="card-group">
@@ -118,9 +108,6 @@
                 <img src="./../images/Poesies/Manon.jpg" class="card-img-top" alt="Réalisation poétique">
             </div>
         </div>
-        </div>
-
-        <div id="border">
         </div>
 
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
