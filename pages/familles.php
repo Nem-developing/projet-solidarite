e234e32fb5e3059394add7061749382f4e161a91
IlanMarrache <65493510+IlanMarrache@users.noreply.github.com>
2020-06-04 20:28:23 +0200

    Ajout d'alias images

--- pages/familles.php
+++ pages/familles.php
@@ -46,49 +46,49 @@
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     
-                    <img src="../images/familles/Allan Pontarelli.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Allan Pontarelli.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Anais et José Gil.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Anais et José Gil.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Audrey Olivier, maman de Lou-Anne Merlen 2d08.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Audrey Olivier, maman de Lou-Anne Merlen 2d08.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Dan.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Dan.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Elsa Laplace.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Elsa Laplace.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Eléonore GRANGER - Docteur Stéphane.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Eléonore GRANGER - Docteur Stéphane.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Fadila, maman de Jahmélia, 2d08.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Fadila, maman de Jahmélia, 2d08.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Baccialone .jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Baccialone .jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Faugière.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Faugière.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Furnon.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Furnon.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Gireme, BTS SIO 2.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Gireme, BTS SIO 2.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Léa Vincent.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Léa Vincent.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Mayo (1ère 7).jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Mayo (1ère 7).jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Nini.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Nini.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
                 <div class="carousel-item">
-                    <img src="../images/familles/Famille Oddoz.jpg" class="d-block w-10" alt="...">
+                    <img src="../images/familles/Famille Oddoz.jpg" class="d-block w-10" alt="Action solidaire des familles">
                 </div>
 
                 
@@ -121,55 +121,55 @@
         <div class="container fond" id="full-back">
         <div class="card-group">
             <div class="card">
-                <img src="./../images/familles/Allan Pontarelli.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Allan Pontarelli.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Anais et José Gil.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Anais et José Gil.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Audrey Olivier, maman de Lou-Anne Merlen 2d08.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Audrey Olivier, maman de Lou-Anne Merlen 2d08.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Dan.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Dan.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Elsa Laplace.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Elsa Laplace.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
         </div>
         
         <div class="card-group">
             <div class="card">
-                <img src="./../images/familles/Eléonore GRANGER - Docteur Stéphane.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Eléonore GRANGER - Docteur Stéphane.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Fadila, maman de Jahmélia, 2d08.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Fadila, maman de Jahmélia, 2d08.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Baccialone .jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Baccialone .jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Faugière.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Faugière.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Furnon.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Furnon.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
         </div>
 
         <div class="card-group">
             <div class="card">
-                <img src="./../images/familles/Famille Gireme, BTS SIO 2.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Gireme, BTS SIO 2.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Léa Vincent.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Léa Vincent.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Mayo (1ère 7).jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Mayo (1ère 7).jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Nini.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Nini.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
             <div class="card">
-                <img src="./../images/familles/Famille Oddoz.jpg" class="card-img-top" alt="...">
+                <img src="./../images/familles/Famille Oddoz.jpg" class="card-img-top" alt="Action solidaire des familles">
             </div>
         </div>
         </div>
