906e0ecd5276b0b44acff0c4de751d156963d3f2
IlanMarrache <65493510+IlanMarrache@users.noreply.github.com>
2020-06-05 00:06:35 +0200

    Changement de lien vers le fichier css

--- pages/communication.php
+++ pages/communication.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="icon" href="../favicon.ico">
         <title>Retour de Presse et communication</title>
-        <link href="../css/css.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/pages.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
 
